8174e4e6f5124b9cfce5126b6a079110149e0f4e
python240419 <51232248+python240419@users.noreply.github.com>
2019-11-15 09:52:26 +0200

    Update flask_basic.py

--- flask_basic.py
+++ flask_basic.py
@@ -1,5 +1,16 @@
 # first - import flask from Settings
-
 from flask import Flask
 from flask import render_template, request, redirect,url_for
 
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    name = 'itay'
+    return f'<html><h1><b>Welcome to flask! {name}</b></h1></html>'
+
+@app.route('/again')
+def again_page():
+    return f'<hr>again...<br><hr>'
+
+app.run()
