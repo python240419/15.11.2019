58cd271b8373191fb3543e604236a06fec6c7e3b
python240419 <51232248+python240419@users.noreply.github.com>
2019-11-15 10:02:45 +0200

    Update flask_basic.py

--- flask_basic.py
+++ flask_basic.py
@@ -13,4 +13,19 @@ def home_page():
 def again_page():
     return f'<hr>again...<br><hr>'
 
+def hello_no_route():
+    # will not work....
+    return f'<hr>hello...<br><hr>'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f'Hello {name}'
+
+@app.route('/blog/<int:postID>')
+def my_blog(postID):
+    post = ['post one', 'second post', 'cool third post']
+    return f'<hr><b>POST :</b> {post[postID]}<br><hr>'
+
+# targil - create URL which gets 2 numbers and print their sum
+# 'sum/3/4' --> will display: 3 + 4 = 7
 app.run()
